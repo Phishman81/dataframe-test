7ce7de7da756384eae5f920f0be0304954b253e7
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-07-12 23:40:13 +0200

    Update main.py-linechart test

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -11,12 +10,19 @@ def load_data(file):
     # Group by page and month_year, and calculate the sum of clicks
     grouped_data = data.groupby(['page', 'month_year'])['clicks'].sum().reset_index()
     
+    # Create a column with clicks history for each page
+    clicks_history = grouped_data.groupby('page')['clicks'].apply(list).reset_index()
+    clicks_history.columns = ['page', 'clicks_history']
+    
     # Create a pivot table with pages as rows and months as columns
     pivot_data = grouped_data.pivot(index='page', columns='month_year', values='clicks').reset_index()
     pivot_data = pivot_data.fillna(0)
     
+    # Merge the pivot table with clicks history
+    pivot_data = pd.merge(pivot_data, clicks_history, on='page')
+
     # Calculate the expected clicks for the current month based on the average of the previous months
-    pivot_data['Expected Clicks in current month'] = pivot_data.iloc[:, 1:-1].mean(axis=1)
+    pivot_data['Expected Clicks in current month'] = pivot_data.iloc[:, 1:-2].mean(axis=1)
     
     # Calculate the deviation for each month
     months = grouped_data['month_year'].unique()
@@ -38,7 +44,15 @@ def main():
         uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
         if uploaded_file is not None:
             data = load_data(uploaded_file)
-            edited_data = st.data_editor(data)  # 👈 An editable dataframe
-                     
+            st.dataframe(
+                data,
+                column_config={
+                    "clicks_history": st.column_config.LineChartColumn(
+                        "Clicks over time"
+                    ),
+                },
+                hide_index=True,
+            )
+
 if __name__ == "__main__":
     main()
